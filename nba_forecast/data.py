f98a40d571eb02e078a83d8b1aae2a3001006441
Malo Meloux <malo.meloux@hotmail.fr>
2021-08-31 12:07:03 +0200

    commit intermédiaire (trainer/model/data/metrics.py)

--- nba_forecast/data.py
+++ nba_forecast/data.py
@@ -13,30 +13,46 @@ def get_data_using_pandas(name_dataset):
     elif name_dataset == 'dataset_draft_2021':
         df = pd.read_csv(f"{CURRENT_PATH}/nba_forecast/data/univ_data_2021.csv")
 
-    elif name_dataset == 'dataset1':
-        df = pd.read_csv(f"{CURRENT_PATH}/nba_forecast/data/")
-
     elif name_dataset == 'train':
         df = pd.read_csv(f"{CURRENT_PATH}/nba_forecast/data/common_dataset.csv")
 
     return df
 
-def filter_data(df,stat):
+def filter_data(df,type):
     #returns a dataset with only the features needed (offensive ratio prediction or defensive ratio prediction)
-    off_features = ['last_uni_age', 'pos', 'per','ts_pct','fg3a_per_fga_pct','fta_per_fga_pct',\
+    off_features = ['gs_pct', 'last_uni_age', 'pos', 'per','ts_pct','fg3a_per_fga_pct','fta_per_fga_pct',\
                     'orb_pct','ast_pct','tov_pct','usg_pct','ows','obpm']
 
-    def_features = ['last_uni_age', 'pos', 'stl_pct','blk_pct','dws','drb_pct','dbpm']
+    def_features = ['gs_pct', 'last_uni_age', 'pos', 'stl_pct','blk_pct','dws','drb_pct','dbpm']
 
     athletics_features = [  'body_fat_pct', 'hand_length', 'hand_width', 'height_wo_shoes',\
                             'height_w_shoes', 'standing_reach', 'weight', 'wingspan']
+    
+    risk_features = [ 'mp','per','ts_pct','efg_pct','fg3a_per_fga_pct','fta_per_fga_pct',
+          'orb_pct','drb_pct','ast_pct','stl_pct','blk_pct','tov_pct','usg_pct','ows','dws','obpm','dbpm','years']
 
-    if stat == 'off':
+    if type == 'model_off':
+        #return dataset with offensive and atheltics features (discard defensive features)
         df = df[off_features+athletics_features]
+        #convert numerical features to float64 to match KNN imputer params
+        to_be_converted = [ 'gs_pct','mp','nb_year_uni','hand_length','hand_width',\
+                            'height_w_shoes','height_wo_shoes','standing_reach','wingspan']
+        for column in to_be_converted:
+            df[column] = df[column].astype('float64')
+
         return df
 
-    elif stat == 'def':
+    elif type == 'model_def':
         #return dataset with defensive and atheltics features (discard offensive features)
         df = df[def_features+athletics_features]
         df = pd.get_dummies(df)
-        return df[def_features+athletics_features]
+        return df
+    
+    elif type == 'model_risk':
+        #return dataset with features used for risk factor prediction
+        df = df[risk_features]
+        return df
+
+if __name__ == '__main__':
+    df = get_data_using_pandas('train')
+    print(df)
--- nba_forecast/model.py
+++ nba_forecast/model.py
@@ -1,19 +1,25 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import LogisticRegression
 
+def get_model(model_type):
+    #build model depending of the parameter 'name_model' 
+    # => offensive rate prediction / defensive rate prediction / risk factor computation
+    if model_type == 'model_off':
+        model = Ridge(
+                    alpha = 1.2,
+                    solver ='sag',
+                    tol = 0
+                    )
 
-
-def get_model(name_model):
-
-    if name_model == 'Ridge':
-        model = Ridge()
-
-    elif name_model == 'off':
+    elif model_type == 'model_def':
         model = RandomForestRegressor(
                     max_depth=1, 
                     min_samples_leaf=4, 
                     min_samples_split=5,
                     n_estimators=15
                     )
+    elif model_type == 'model_risk':
+        model = LogisticRegression(max_iter=300)
 
     return model
